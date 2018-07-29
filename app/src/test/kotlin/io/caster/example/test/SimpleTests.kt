7fc04d9fe24b6d31b250872cfc2e3d8ae41e83a1
Marcel Schnelle <marcel@mytenten.com>
2018-07-29 14:57:23 +0200

    Use distinct app module to unify Java & Android project structure

--- app/src/test/kotlin/io/caster/example/test/SimpleTests.kt
+++ app/src/test/kotlin/io/caster/example/test/SimpleTests.kt
@@ -3,12 +3,20 @@ package io.caster.example.test
 import io.caster.example.Card
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
 
-class ModelTests {
+class Tests {
 
     @Test
-    fun exampleTest() {
+    fun simpleTest(info: TestInfo) {
+        println("What now: $info")
         val model = Card(suit = "♤", value = "7")
         assertEquals("7♤", model.toString())
+    }
+
+    @Test
+    fun anotherTest(info: TestInfo) {
+        println("My test: $info")
+        assertEquals(12, 6 + 6)
     }
 }
