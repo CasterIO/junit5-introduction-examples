3041fae92f7c07ba2fd6021bc4a9c15ae880afa4
Marcel Schnelle <marcelschnelle@aol.com>
2018-08-04 23:30:38 +0200

    Add some more unit tests to showcase different Assertion methods

--- app/src/test/java/io/caster/example/test/DeckTests.kt
+++ app/src/test/java/io/caster/example/test/DeckTests.kt
@@ -2,14 +2,16 @@ package io.caster.example.test
 
 import io.caster.example.Card
 import io.caster.example.Deck
-import io.caster.example.Rank.Ace
-import io.caster.example.Rank.Num
+import io.caster.example.EmptyDeckException
+import io.caster.example.Rank.*
 import io.caster.example.Suit.Hearts
 import io.caster.example.Suit.Spades
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("Integrity Tests for Deck objects")
 class DeckTests {
@@ -28,5 +30,26 @@ class DeckTests {
         val unrelatedCard = Card(rank = Num(7), suit = Hearts)
         val deck = Deck(listOf(containedCard))
         assertFalse(unrelatedCard in deck)
+    }
+
+    @Test
+    @DisplayName("Drawing from an empty deck throws an Exception")
+    fun drawingFromEmptyDeckThrowsException() {
+        val deck = Deck(cards = emptyList())
+        assertThrows<EmptyDeckException> {
+            deck.draw()
+        }
+    }
+
+    @Test
+    @DisplayName("A Deck properly reports cards that it actually contains")
+    fun cardsThatAreContainedInTheDeckAreCorrectlyReported() {
+        val card1 = Card(rank = Ace, suit = Spades)
+        val card2 = Card(rank = Queen, suit = Hearts)
+        val deck = Deck(listOf(card1, card2))
+        assertAll("Check that all cards are contained in the deck",
+                { assertTrue(card1 in deck) },
+                { assertTrue(card2 in deck) }
+        )
     }
 }
